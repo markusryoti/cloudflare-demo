a446184b0307df67dfd990fd1dcf01aff3d0b1a1
Markus Ryöti <mryoti@gmail.com>
2025-11-04 23:58:01 +0200

    add vue query

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-fetch('http://localhost:8787/todos')
-  .then((res) => res.json())
-  .then((data) => console.log(data))
+import { useQuery } from '@tanstack/vue-query'
+
+interface Todo {
+  id: string
+  title: string
+  completed: boolean
+}
+
+const { data, isLoading, error } = useQuery({
+  queryKey: ['todos'],
+  queryFn: () =>
+    fetch('http://localhost:8787/todos')
+      .then((res) => res.json() as Promise<{ todos: Todo[] }>)
+      .then((data) => data.todos),
+})
 </script>
 
 <template>
-  <main>Hey</main>
+  <main>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="error">An error occurred: {{ error.message }}</div>
+    <pre v-else>{{ data }}</pre>
+  </main>
 </template>
